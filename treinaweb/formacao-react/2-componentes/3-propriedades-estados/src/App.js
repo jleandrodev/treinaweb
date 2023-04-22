a4882ab52d3e04a83eecb3dd939e40133d62348d
Ozório <jleandro.dev@gmail.com>
2023-04-22 11:26:13 -0300

    Finalização do curso, inicio do projeto TreinaMusic

--- treinaweb/formacao-react/2-componentes/3-propriedades-estados/src/App.js
+++ treinaweb/formacao-react/2-componentes/3-propriedades-estados/src/App.js
@@ -1,19 +1,46 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useState, useContext, createContext } from 'react';
 import './App.css';
+
+const MeuContexto = createContext()
 
 function App() {
 
-  const meuVideo = useRef()
-
-  useEffect(() => {
-    console.log(meuVideo.current)
-  }, [])
-
+  const [ contador, setContador ] = useState(0)
   return (
-    <div className="App">
-      <video ref={meuVideo} />
-    </div>
+    <MeuContexto.Provider value={{qualquer: contador}}>
+      <div className="App">
+        <button onClick={() => setContador(contador + 1)}>
+          CLICK 1 - {contador}
+        </button>
+        <Treinaweb  />
+      </div>
+    </MeuContexto.Provider>
   );
 }
+
+
+function Treinaweb(props) {
+
+
+  return (
+    <React.Fragment>
+      <h2>Treinaweb - </h2>
+      <OutroComponente />
+    </React.Fragment>
+  )
+}
+
+function OutroComponente(props) {
+
+  const { qualquer } = useContext(MeuContexto)
+
+  return (
+    <React.Fragment>
+      <h2>OutroComponente - {qualquer}</h2>
+    </React.Fragment>
+  )
+}
+
+
 
 export default App;
