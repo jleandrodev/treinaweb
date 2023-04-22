71b419a78734e4bb28bcc80a529ad6e314ed6ff4
Ozório <jleandro.dev@gmail.com>
2023-04-22 08:32:53 -0300

    Fragmentos - segundo modo

--- treinaweb/formacao-react/2-componentes/3-propriedades-estados/src/App.js
+++ treinaweb/formacao-react/2-componentes/3-propriedades-estados/src/App.js
@@ -1,15 +1,16 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function Lista(props) {
 
 
   return (
-    <>
+    // Proporciona passar parâmetros para os framgmentos
+    <React.Fragment>
       <li>Karen</li>
       <li>Maria</li>
       <li>João</li>
-    </>
+    </React.Fragment>
   )
 }
 
