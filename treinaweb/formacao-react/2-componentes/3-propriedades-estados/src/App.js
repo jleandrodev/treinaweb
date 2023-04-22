577657b2442fbcd8b71f6aad281037323062cc4f
Ozório <jleandro.dev@gmail.com>
2023-04-22 10:48:05 -0300

    Criando Referências

--- treinaweb/formacao-react/2-componentes/3-propriedades-estados/src/App.js
+++ treinaweb/formacao-react/2-componentes/3-propriedades-estados/src/App.js
@@ -1,29 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 import './App.css';
-
-function Lista(props) {
-
-
-  return (
-    // Proporciona passar parâmetros para os framgmentos
-    <React.Fragment>
-      <li>Karen</li>
-      <li>Maria</li>
-      <li>João</li>
-      {props.children}
-    </React.Fragment>
-  )
-}
 
 function App() {
 
+  const meuVideo = useRef()
+
+  useEffect(() => {
+    console.log(meuVideo.current)
+  }, [])
+
   return (
     <div className="App">
-      <ul>
-        <Lista>
-          <h2>Contador</h2>
-        </Lista>
-      </ul>
+      <video ref={meuVideo} />
     </div>
   );
 }
