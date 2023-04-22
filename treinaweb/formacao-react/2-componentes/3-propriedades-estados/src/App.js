a75509151e659580247056d8920f735908e7284d
Ozório <jleandro.dev@gmail.com>
2023-04-22 08:29:43 -0300

    Fragmentos

--- treinaweb/formacao-react/2-componentes/3-propriedades-estados/src/App.js
+++ treinaweb/formacao-react/2-componentes/3-propriedades-estados/src/App.js
@@ -1,50 +1,24 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 
-function Treinaweb(props) {
+function Lista(props) {
 
-  useEffect(() => {
-    const meuIntervalo = setInterval(() => {
-      console.log("Olá")
-    }, 2000)
-    console.log('Componente criado')
-    return () => {
-      clearInterval(meuIntervalo)
-      console.log('Componente removido')
-    }
-  }, [])
+
   return (
-    <div>
-      <span>{props.nome}</span>
-    </div>
+    <>
+      <li>Karen</li>
+      <li>Maria</li>
+      <li>João</li>
+    </>
   )
 }
 
 function App() {
 
-  const minhaLista = [
-    {id: 1, nome: 'Jhone', idade: '30'},
-    {id: 2, nome: 'Jose', idade: '54'},
-    {id: 3, nome: 'Mauricio', idade: '20'},
-    {id: 4, nome: 'Leticia', idade: '25'},
-  ]
-
-  function falarNome (nome) {
-    console.log(nome)
-  }
-  
   return (
     <div className="App">
       <ul>
-        {
-          minhaLista.map(el => {
-            return (
-            <li key={el.id} onClick={() => falarNome(el.nome)}>
-              Nome: {el.nome}, idade: {el.idade}
-            </li>
-            )
-          })
-        }
+        <Lista />
       </ul>
     </div>
   );
