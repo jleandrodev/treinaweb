e83c82f65627b5d6d934b5a27723db4471fe45d6
Ozório <jleandro.dev@gmail.com>
2023-04-26 18:10:51 -0300

    useRef - Criação do Player de video

--- treinaweb/formacao-react/3-hooks/react-hooks/src/App.js
+++ treinaweb/formacao-react/3-hooks/react-hooks/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import NewVideoForm from './ui/components/NewVideoForm';
 import VideoList from './ui/components/VideoList';
+import VideoPlayer from './ui/components/VideoPlayer';
 
 function App() {
   return (
     <div className="App">
       <NewVideoForm />
+      <VideoPlayer />
       <VideoList />
     </div>
   );
---
+++ treinaweb/formacao-react/3-hooks/react-hooks/src/ui/components/VideoPlayer.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { TimeService } from '../../data/services/TimeService'
+
+const _selectedVideo = {
+  id: 1,
+  title: 'Como fazer um bolo de chocolate',
+  duration: 12,
+  url: 'https://joy1.videvo.net/videvo_files/video/free/2014-12/small_watermarked/Raindrops_Videvo_preview.webm',
+  cover: 'https://images.freeimages.com/images/previews/fca/peppers-falling-into-water-1142701.jpg'
+}
+
+export default function VideoPlayer() {
+  const video = _selectedVideo
+  return (
+    <div className='video-player'>
+      <video src={video.url} />
+      <div className='controls'>
+        <button></button>
+        <span>10:20 / {TimeService.formatTime(video.duration)}</span>
+        <input type="range" min={0} max={video.duration} step={.1} />
+      </div>
+      <h2>{video.title}</h2>
+    </div>
+  )
+}
