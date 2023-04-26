a5eb0ec761b7959f4e4b31ff846d3eb5c14c9fc8
Ozório <jleandro.dev@gmail.com>
2023-04-26 18:24:29 -0300

    useRef - Play e pause implementados

--- treinaweb/formacao-react/3-hooks/react-hooks/src/ui/components/VideoPlayer.jsx
+++ treinaweb/formacao-react/3-hooks/react-hooks/src/ui/components/VideoPlayer.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import { TimeService } from '../../data/services/TimeService'
 
 const _selectedVideo = {
@@ -10,12 +10,47 @@ const _selectedVideo = {
 }
 
 export default function VideoPlayer() {
+
   const video = _selectedVideo
+  const videoRef = useRef()
+  const [ isPlaying, setPlay ] = useState(false)
+
+  useEffect(() => {
+    const videoElement = videoRef.current
+
+    videoElement.addEventListener('play', play)
+    videoElement.addEventListener('pause', pause)
+    videoElement.addEventListener('seeked', progress)
+
+    return () => {
+      videoElement.removeEventListener('play', play)
+      videoElement.removeEventListener('pause', pause)
+      videoElement.removeEventListener('seeked', progress)
+    }
+  }, [video])
+
+  function play() {
+    videoRef.current.play()
+    setPlay(true)
+  }
+
+  function pause() {
+    videoRef.current.pause()
+    setPlay(false)
+  }
+
+  function progress() {
+
+  }
+
+
   return (
     <div className='video-player'>
-      <video src={video.url} />
+      <video src={video.url} ref={videoRef} />
       <div className='controls'>
-        <button></button>
+        <button onClick={isPlaying ? pause : play}>
+          { isPlaying ? '||' : '|>' }
+        </button>
         <span>10:20 / {TimeService.formatTime(video.duration)}</span>
         <input type="range" min={0} max={video.duration} step={.1} />
       </div>
