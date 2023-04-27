9185914ff66c6aa8b4758400a61775a6e7eda751
Ozório <jleandro.dev@gmail.com>
2023-04-27 08:51:20 -0300

    useRef - Finalização do player de video

--- treinaweb/formacao-react/3-hooks/react-hooks/src/ui/components/VideoPlayer.jsx
+++ treinaweb/formacao-react/3-hooks/react-hooks/src/ui/components/VideoPlayer.jsx
@@ -4,8 +4,8 @@ import { TimeService } from '../../data/services/TimeService'
 const _selectedVideo = {
   id: 1,
   title: 'Como fazer um bolo de chocolate',
-  duration: 12,
-  url: 'https://joy1.videvo.net/videvo_files/video/free/2014-12/small_watermarked/Raindrops_Videvo_preview.webm',
+  duration: 23,
+  url: 'https://joy1.videvo.net/videvo_files/video/free/2019-07/small_watermarked/190625_04_CityMisc_HD_05_preview.webm',
   cover: 'https://images.freeimages.com/images/previews/fca/peppers-falling-into-water-1142701.jpg'
 }
 
@@ -14,20 +14,34 @@ export default function VideoPlayer() {
   const video = _selectedVideo
   const videoRef = useRef()
   const [ isPlaying, setPlay ] = useState(false)
+  const [ progress, setProgress ] = useState(0)
+  const progressTimer = useRef()
 
   useEffect(() => {
     const videoElement = videoRef.current
 
     videoElement.addEventListener('play', play)
     videoElement.addEventListener('pause', pause)
-    videoElement.addEventListener('seeked', progress)
+    videoElement.addEventListener('seeked', onProgress)
 
     return () => {
       videoElement.removeEventListener('play', play)
       videoElement.removeEventListener('pause', pause)
-      videoElement.removeEventListener('seeked', progress)
+      videoElement.removeEventListener('seeked', onProgress)
+      setTime(0)
+      pause()
     }
   }, [video])
+
+  useEffect(() => {
+    clearInterval(progressTimer.current)
+    if(isPlaying){
+
+      progressTimer.current = setInterval(onProgress, 1000)
+    
+    }
+
+  }, [isPlaying])
 
   function play() {
     videoRef.current.play()
@@ -39,22 +53,48 @@ export default function VideoPlayer() {
     setPlay(false)
   }
 
-  function progress() {
+  function onProgress() {
+    setProgress(videoRef.current.currentTime)
+  }
 
+  function onChangeProgress(event) {
+    setTime(event.target.value)
+  }
+
+  function setTime(time) {
+    videoRef.current.currentTime = time
+    onProgress()
   }
 
 
   return (
     <div className='video-player'>
-      <video src={video.url} ref={videoRef} />
-      <div className='controls'>
-        <button onClick={isPlaying ? pause : play}>
-          { isPlaying ? '||' : '|>' }
-        </button>
-        <span>10:20 / {TimeService.formatTime(video.duration)}</span>
-        <input type="range" min={0} max={video.duration} step={.1} />
-      </div>
-      <h2>{video.title}</h2>
+      <video 
+        src={video.url} 
+        ref={videoRef}
+        loop={false} 
+      />
+      {video.url && (
+        <>
+        <div className='controls'>
+          <button onClick={isPlaying ? pause : play}>
+            { isPlaying ? '||' : '|>' }
+          </button>
+          <span>{TimeService.formatTime(Math.round(progress))} / {TimeService.formatTime(video.duration)}</span>
+          <input 
+            type="range" 
+            value={progress} 
+            min={0} 
+            max={video.duration} 
+            step={.1} 
+            onChange={onChangeProgress}
+          />
+        </div>
+        <h2>{video.title}</h2>
+        
+        </>
+
+      )}
     </div>
   )
 }
