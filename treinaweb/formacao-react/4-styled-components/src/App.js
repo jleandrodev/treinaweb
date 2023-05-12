32417ff1948b3a9f29a9158214c6dd770c3805ee
Ozório <jleandro.dev@gmail.com>
2023-05-12 07:57:47 -0300

    Styled components - modificando tema com clique

--- treinaweb/formacao-react/4-styled-components/src/App.js
+++ treinaweb/formacao-react/4-styled-components/src/App.js
@@ -1,31 +1,23 @@
-import { useState } from 'react';
 import './App.css';
 import styled from '@emotion/styled/macro';
-import { keyframes } from '@emotion/react';
+import { ThemeProvider } from '@emotion/react';
+import { useState } from 'react';
 
-const sumir = keyframes`
-  0%{
-    opacity: 1;
-  }
-  100%{
-    opacity: 0%;
-  }
-`
+const tema1 = {
+  corDaFonte: 'white',
+  corDeFundo: 'green'
+}
 
-const MeuSpan = styled('span')``
+const tema2 = {
+  corDaFonte: 'green',
+  corDeFundo: 'yellow'
+}
 
-const MinhaDiv = styled.div`
 
-  background-color: green;
-  color: ${({ ligado }) => ligado ? 'white' : 'red'};
+const MinhaDiv = styled('div')`
 
-   &:hover ${MeuSpan}{
-    color: yellow;
-    animation: ${sumir} 1s ease alternate-reverse infinite;
-    @media (max-width: 200px) {
-      color: blue
-    }
-  }
+  background-color: ${({ theme }) => theme.corDeFundo};
+  color: ${({ theme }) => theme.corDaFonte};
 
 `
 
@@ -33,14 +25,13 @@ const MinhaDiv = styled.div`
 function App() {
   const [ ligado, setLigado ] = useState(true)
   return (
-    <div className="App" onClick={() => setLigado(!ligado)}>
-      <MinhaDiv ligado={ligado} >
-        Treinaweb
-        <MeuSpan>
-          Teste
-        </MeuSpan>
-      </MinhaDiv>
-    </div>
+    <ThemeProvider theme={ligado ? tema1 : tema2}>
+      <div className="App" onClick={() => setLigado(!ligado)}>
+        <MinhaDiv>
+          Treinaweb
+        </MinhaDiv>
+      </div>
+    </ThemeProvider>
   );
 }
 
