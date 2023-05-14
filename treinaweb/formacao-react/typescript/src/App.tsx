06c41af0a780746e3d3d269bd8dd73b2da320597
Ozório <jleandro.dev@gmail.com>
2023-05-14 07:37:40 -0300

    Typescript - Tipagem de estados

--- treinaweb/formacao-react/typescript/src/App.tsx
+++ treinaweb/formacao-react/typescript/src/App.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 window.treinaweb.start()
 
 export interface MeuComponenteProps {
   name?: string
-  children: JSX.Element | JSX.Element[] | string
+  children?: JSX.Element | JSX.Element[] | string
+}
+
+export interface User {
+  name: string
+  age: number
 }
 
 function MeuComponente ({name} : MeuComponenteProps) {
+
+  const [counter, setCounter] = useState<number>()
+  const [user, setUser] = useState<User>({} as User)
+
   return (
     <h1>Hello</h1>
   )
