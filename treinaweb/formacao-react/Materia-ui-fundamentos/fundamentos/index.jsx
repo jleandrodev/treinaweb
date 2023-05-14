42bf524edcdc1de359ed2411b47bd60193897193
Ozório <jleandro.dev@gmail.com>
2023-05-14 08:48:58 -0300

    React - Material UI - Configuração

---
+++ treinaweb/formacao-react/Materia-ui-fundamentos/fundamentos/index.jsx
@@ -0,0 +1,12 @@
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const { Typography } = MaterialUI
+
+function MeuP () {
+  return <Typography>Olá mundo</Typography>
+}
+
+root.render(
+  <MeuP />
+
+  )
