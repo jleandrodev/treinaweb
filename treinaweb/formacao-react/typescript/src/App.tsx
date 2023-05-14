4efd9234c80451ac93470fb4faae72dd3fd34ff8
Ozório <jleandro.dev@gmail.com>
2023-05-14 07:28:50 -0300

    Typescript - Declaração de props

--- treinaweb/formacao-react/typescript/src/App.tsx
+++ treinaweb/formacao-react/typescript/src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+window.treinaweb.start()
+
+export interface MeuComponenteProps {
+  name: string
+}
+
+function MeuComponente ({name} : MeuComponenteProps) {
+  return (
+    <h1>Hello</h1>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MeuComponente name='John Doe' />
     </div>
   );
 }
---
+++ treinaweb/formacao-react/typescript/src/data/@types/3rd/abc.d.ts
@@ -0,0 +1,5 @@
+interface Window {
+  treinaweb: {
+    start: () => void
+  }
+}
