7cdce7d185592eaf494c057297ca7af2ce55c01f
Ozório <jleandro.dev@gmail.com>
2023-05-18 07:13:25 -0300

    Material UI - Fundamentos - Componentes

--- treinaweb/formacao-react/Materia-ui-fundamentos/fundamentos/index.jsx
+++ treinaweb/formacao-react/Materia-ui-fundamentos/fundamentos/index.jsx
@@ -1,12 +1,22 @@
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-const { Typography } = MaterialUI
+const { Typography, Button  } = MaterialUI
 
 function MeuP () {
   return <Typography>Olá mundo</Typography>
 }
 
+function MyButton() {
+  return <button>Clique Aqui</button>
+}
+
 root.render(
-  <MeuP />
+  <>
+    <MeuP />
+    <MyButton />
+    <Button sx={{ m: "8px" }}>Click</Button>
+    <Button sx={{ m: 2 }} variant="outlined">Click</Button>
+    <Button variant="contained">Click</Button>
+  </>
 
   )
