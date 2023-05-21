8340c58895a5468ce4dd62eecefcdf2b9782495d
Ozório <jleandro.dev@gmail.com>
2023-05-21 09:59:36 -0300

    PHP - Fundamentos - excessões

--- treinaweb/formacao-php/php-fundamentos/erros.php
+++ treinaweb/formacao-php/php-fundamentos/erros.php
@@ -4,4 +4,17 @@ error_reporting(E_ALL);
 
 // notice
 
+$divisor = 0;
+
+
+try {
+  if ($divisor == 0) {
+    throw new Exception("Não é possível dividir por zero", 1);
+  }
+  $valor = 100 / $divisor;
+
+} catch(\Throwable $th) {
+  echo $th->getMessage();
+}
+
 
