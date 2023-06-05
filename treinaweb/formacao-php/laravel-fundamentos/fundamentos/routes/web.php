e32142aa12c1ddb06050c35c9a7c775c241d5108
Ozório <jleandro.dev@gmail.com>
2023-06-05 12:10:27 -0300

    feat(Laravel-fundamentos): Entendendo o conceito de rodas

--- treinaweb/formacao-php/laravel-fundamentos/fundamentos/routes/web.php
+++ treinaweb/formacao-php/laravel-fundamentos/fundamentos/routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "Home";
+});
+
+Route::get('/services', function () {
+    echo "Serviços";
 });
